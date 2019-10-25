dc1fc554e31eac72c0213de361ccc4a79ecdc2ab
mingzhe.zhang <1084904209@qq.com>
2019-10-25 11:10:41 +0800

    chang text

--- template/src/index.vue
+++ template/src/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper">
     <image :src="logo" class="logo" />
-    <text class="greeting">The environment is ready!</text>
+    <text class="greeting">ZMZ</text>
     {{#router}}
     <router-view/>
     {{else}}
